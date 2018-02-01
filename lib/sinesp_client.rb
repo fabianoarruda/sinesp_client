793db8b1e937e4a4b40b342f2749fbcfdc8b5b7f
Fabiano Arruda <fabianoarruda@gmail.com>
2018-01-31 23:45:42 -0200

    Fixed to work with last API version

--- lib/sinesp_client.rb
+++ lib/sinesp_client.rb
@@ -1,11 +1,11 @@
 require "sinesp_client/version"
+require 'net/http'
+require 'uri'
 require 'openssl'
-require 'httparty'
 require 'securerandom'
+require 'nokogiri'
 
 module SinespClient
-
-  include HTTParty
 
   def self.lat
     rand(-90.000000000...90.000000000)
@@ -17,54 +17,70 @@ module SinespClient
 
   def self.search(plate)
 
-    key = "TRwf1iBwvCoSboSscGne"
+    key = "#8.1.0#Mw6HqdLgQsX41xAGZgsF"
     digest = OpenSSL::Digest.new('sha1')
     hash = OpenSSL::HMAC.hexdigest(digest, plate + key, plate)
 
-    body = "<?xml version='1.0' encoding='utf-8'?>
-                <soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">
-                  <soap:Header>
-                  <b>motorola XT1078</b>
-                  <c>ANDROID</c>
-                  <i>#{lat}</i>
-                  <m>8797e74f0d6eb7b1ff3dc114d4aa12d3</m>
-                  <e>4.1.1</e>
-                  <f>10.0.0.1</f>
-                  <g>#{hash}</g>
-                  <d>6.0</d>
-                  <h>#{long}</h>
-                  <k />
-                  <l>#{Time.now}</l>
-                  </soap:Header>
-                  <soap:Body>
-                    <webs:getStatus xmlns:webs=\"http://soap.ws.placa.service.sinesp.serpro.gov.br/\">
-                    <a>#{plate}</a>
-                    </webs:getStatus>
-                  </soap:Body>
-                </soap:Envelope>"
+    body = "<v:Envelope xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:d=\"http://www.w3.org/2001/XMLSchema\" xmlns:c=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:v=\"http://schemas.xmlsoap.org/soap/envelope/\">
+             <v:Header>
+              <b>Google Android SDK built for x86</b>
+              <c>ANDROID</c>
+              <d>8.1.0</d>
+              <e>4.3.2</e>
+              <f>10.0.2.15</f>
+              <g>#{hash}</g>
+              <h>#{long}</h>
+              <i>#{lat}</i>
+              <k />
+              <l>#{Time.now}</l>
+              <m>8797e74f0d6eb7b1ff3dc114d4aa12d3</m>
+             </v:Header>
+             <v:Body>
+              <n0:getStatus xmlns:n0=\"http://soap.ws.placa.service.sinesp.serpro.gov.br/\">
+               <a>#{plate}</a>
+              </n0:getStatus>
+             </v:Body>
+            </v:Envelope>"
 
-    headers = { "Content-type" => "text/xml; charset=utf-8",
-                "Timeout" => "30000",
+
+    headers = { "Content-type" => "application/x-www-form-urlencoded; charset=UTF-8",
+                "Accept" => "text/plain, */*; q=0.01",
+                "Cache-Control" => "no-cache",
+                "Pragma" => "no-cache",
                 "Content-length" => body.length.to_s,
-                "User-Agent" => "SinespCidadao / 3.0.2.1 CFNetwork / 758.2.8 Darwin / 15.0.0"
+                "User-Agent" => "SinespCidadao/4.2.3 CFNetwork/808.1.4 Darwin/16.1.0"
     }
-
 
     #begin
 
-      response = post("http://sinespcidadao.sinesp.gov.br/sinesp-cidadao/mobile/consultar-placa", body: body, headers: headers, timeout: 10)
+    uri = URI.parse("https://189.9.194.154/sinesp-cidadao/mobile/consultar-placa/v3")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(uri.request_uri, headers)
+    request.body = body
+
+    response = http.request(request).body
 
     #rescue
     #  return nil
     #end
 
+    dom = Nokogiri::XML(response)
+
+    node = dom.at_xpath('//return')
+
+    parsed_response = node.element_children.each_with_object(Hash.new) do |e, h|
+      h[e.name.to_sym] = e.content
+    end
 
     #If the query is valid, "codigoRetorno" will be 0. Otherwise something went wrong. Just return nil.
-    #if response["Envelope"]["Body"]["getStatusResponse"]["return"]["codigoRetorno"] == "0"
-      response["Envelope"]["Body"]["getStatusResponse"]["return"]
-    #else
-    #  nil
-    #end
+    # if parsed_response[:codigoRetorno] == "0"
+    #   parsed_response
+    # else
+    #   nil
+    # end
 
   end
 
